5def9e21a8d2ea961c23d5c51cc97d7679daa837
Ruipeng <ruipeng.r.dong@cn.pwc.com>
2024-04-06 22:55:51 +0800

    feat : TDD

---
+++ dotNetParadise-Xunit/dotNetParadise-TDD.Test/MathCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace dotNetParadise_TDD.Test;
+
+public class MathCalculator
+{
+    public int DoubleNumber(int number)
+    {
+        //throw new NotImplementedException();
+        return number * 2;
+    }
+}
---
+++ dotNetParadise-Xunit/dotNetParadise-TDD.Test/MathCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿namespace dotNetParadise_TDD.Test;
+
+public class MathCalculatorTests
+{
+    //[Fact]
+    //public void Add_TwoNumbers_ReturnSum()
+    //{
+    //    // Arrange
+    //    var calculator = new MathCalculator();
+
+    //    // Act
+    //    var result = calculator.Add(3, 5);
+
+    //    // Assert
+    //    Assert.Equal(8, result);
+    //}
+
+
+    [Fact]
+    public void DoubleNumber_WhenGivenSingleNumber_ReturnsDouble()
+    {
+        // Arrange
+        var calculator = new MathCalculator();
+
+        // Act
+        var result = calculator.DoubleNumber(2);
+
+        // Assert
+        Assert.Equal(4, result);
+    }
+}
